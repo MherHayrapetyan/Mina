b03dafab221e4b829bb1526cfdba611abfe1855f
MherHayrapetyan <hayrapetyanmher64@gmail.com>
2023-05-02 19:52:40 +0400

    sectionTwo and sectionThree

--- script.js
+++ script.js
@@ -19,3 +19,36 @@ function burgHovTog() {
 function burgTog() {
   burg.classList.toggle("heigth");
 }
+
+// Section Two
+
+const sectionTwoTxt = document.querySelector(".sectionTwoTxt");
+const sectionTwoTxtTop = document.querySelector(".sectionTwoTxtTop");
+const sectionTwoBox = document.querySelector(".sectionTwoBox");
+const sectionTwoBoxTop = document.querySelector(".sectionTwoBoxTop");
+
+addEventListener("scroll", () => {
+  if (window.pageYOffset > sectionTwoTxtTop.offsetTop - 500) {
+    sectionTwoTxt.style.display = "block";
+    sectionTwoTxtTop.style.display = "none";
+  }
+});
+
+addEventListener("scroll", () => {
+  if (window.pageYOffset > sectionTwoBoxTop.offsetTop - 500) {
+    sectionTwoBox.style.display = "block";
+    sectionTwoBoxTop.style.display = "none";
+  }
+});
+
+// Section Three
+
+const sectionThreeAnim = document.querySelector(".sectionThreeAnim");
+const sectionThreeAnimTop = document.querySelector(".sectionThreeAnimTop");
+
+addEventListener("scroll", () => {
+  if (window.pageYOffset > sectionThreeAnimTop.offsetTop - 500) {
+    sectionThreeAnim.style.display = "block";
+    sectionThreeAnimTop.style.display = "none";
+  }
+});
